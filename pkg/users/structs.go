022411fe4cebecbeff646c9ba1361dba46392999
Charles Pretzer <charles@charlespretzer.com>
2021-01-06 22:20:20 -0800

    Fix password string type
    Signed-off-by: Charles Pretzer <charles@charlespretzer.com>

--- pkg/users/structs.go
+++ pkg/users/structs.go
@@ -6,7 +6,7 @@ type User struct {
 	FirstName string `json:"first_name,omitempty"`
 	LastLogin int64 `json:"last_login,omitempty"`
 	LastName string `json:"last_name,omitempty"`
-	Password int64 `json:"password,omitempty"`
+	Password string `json:"password,omitempty"`
 	Reports  string `json:"reports,omitempty"` // this is a list of report IDs
 	Username string `json:"username,omitempty"`
 }
